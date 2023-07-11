c68ff84298c4a72b4ec69dd99306453f84aec14d
Mustafa Fawzy <hamouda777744@gmail.com>
2023-07-11 17:41:08 +0300

    create custom text field

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:internship_auth_mobile_app/screens/widgets/cover_and_logo.dart';
+import 'package:internship_auth_mobile_app/screens/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -14,6 +15,24 @@ class LoginScreen extends StatelessWidget {
         child: const Column(
           children: [
             CoverAndLogo(),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 12),
+              child: Text(
+                'Log in to your account',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xff2A2B2E),
+                ),
+              ),
+            ),
+            CustomTextField(
+              category: 'Username',
+              hint: 'Enter Your Email',
+              icon: Icons.cancel,
+            ),
           ],
         ),
       ),
---
+++ lib/screens/widgets/custom_text_field.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField(
+      {super.key,
+      required this.category,
+      required this.hint,
+      required this.icon});
+
+  final String category;
+  final String hint;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            category,
+            style: const TextStyle(
+              fontFamily: 'Inter',
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff808194),
+            ),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          SizedBox(
+            height: 44,
+            width: 345,
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: hint,
+                contentPadding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 12,
+                ),
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide(
+                    width: 0.5,
+                    color: Color(0xff808194),
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8),
+                  ),
+                ),
+                suffixIcon: Icon(
+                  icon,
+                  size: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
